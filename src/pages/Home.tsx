d912e0f068c61d0c9e2782792795ff06132b2f61
立减金 <langnadu1991@icloud.com>
2025-08-28 20:49:02 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
-import Navbar from "../components/Navbar";
-import ProductList from "../components/ProductList";
+import { useEffect, useState } from "react";
+import { getProducts } from "../api/api";
+import ProductCard from "../components/ProductCard";
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+}
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getProducts().then(setProducts).catch(console.error);
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <ProductList />
-    </>
+    <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+      {products.map((p) => (
+        <ProductCard key={p.id} product={p} />
+      ))}
+    </div>
   );
 }
